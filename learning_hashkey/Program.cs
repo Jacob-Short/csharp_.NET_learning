0d08a745f1a1cc194e3caa735eee10225bdf63f2
Jacob-Short <thatmanjshort@gmail.com>
2021-09-06 19:52:39 -0700

    created hashset, a list that optimizes set and removed duplicates

---
+++ learning_hashkey/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace learning_hashkey
+{
+    class Program
+    {
+        // hashes out duplicates
+        static void Main(string[] args)
+        {
+            var myHash = new HashSet<String>();
+
+            myHash.Add("hello");
+            myHash.Add("hello");
+            myHash.Add("goodbye");
+
+            // can check if a array has same item as in hashset
+
+            String[] s = new String[] {"hello"};
+
+            System.Console.WriteLine(myHash.Count);
+            System.Console.WriteLine(myHash.Overlaps(s));
+        }
+    }
+}
