4b1b38739caef53524678a32e61f7a6a6fba5a39
Jacob-Short <thatmanjshort@gmail.com>
2021-09-06 19:38:32 -0700

    created stack, can pop off or peek for returning without removing

---
+++ learning_stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace learning_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<String> pancakes = new Stack<string>();
+
+            pancakes.Push("first pancake made");
+            pancakes.Push("second pancake made");
+            pancakes.Push("third pancake made");
+            pancakes.Push("last pancake made");
+
+            // foreach (var pancake in pancakes)
+            // {
+            //     System.Console.WriteLine(pancake);
+            // }
+
+            // removes last item and returns in
+            System.Console.WriteLine(pancakes.Pop());
+
+            // return last item but does not remove it
+            System.Console.WriteLine(pancakes.Peek());
+            System.Console.WriteLine(pancakes.Peek());
+
+    }
+    }
+
+}
