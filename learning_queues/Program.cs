4b57a0d793cb832ae79179e6ebc992390432fc74
Jacob-Short <thatmanjshort@gmail.com>
2021-09-06 19:44:47 -0700

    adding and iterating through a Queue, first in -> first out

---
+++ learning_queues/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace learning_queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> myQueue = new Queue<int>();
+
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(3);
+            myQueue.Enqueue(4);
+
+            foreach (var item in myQueue)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            // similar to popping
+            System.Console.WriteLine(myQueue.Dequeue());
+            System.Console.WriteLine(myQueue.Peek());
+        }
+    }
+}
